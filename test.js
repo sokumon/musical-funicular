47e93ae8683bcddbc3a3b383a7966feed13570da
sokumon <sohamkulkarns9@gmail.com>
2021-08-24 21:40:54 +0530

    first commit 4

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-
-if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register('sw.js');
-}else{
-
-}
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    registrations[0].showNotification("HELLO",{body:"Im sleepinh"});
+    self.addEventListener('notificationonclick', function(event) {
+        event.notification.close();
+        clients.openWindow("https://youtu.be/PAvHeRGZ_lA");
+    }, false);
+  });
 var url=new URL(window.location.href);
 if(url.searchParams.get("subject")!=null && url.searchParams.get("work") && url.searchParams.get("number")){
     document.getElementById("subject").innerText=url.searchParams.get("subject");
     document.getElementById("work").innerText=url.searchParams.get("work")+" "+url.searchParams.get("number");
-
 }
