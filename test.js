90be2599da920cae8d73f56f2af476418a1443a1
sokumon <sohamkulkarns9@gmail.com>
2021-08-24 22:48:45 +0530

    first commit 5

--- test.js
+++ test.js
@@ -1,12 +1,28 @@
-navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    registrations[0].showNotification("HELLO",{body:"Im sleepinh"});
-    self.addEventListener('notificationonclick', function(event) {
-        event.notification.close();
-        clients.openWindow("https://youtu.be/PAvHeRGZ_lA");
-    }, false);
-  });
+//     navigator.serviceWorker.getRegistrations().then(function(registrations) {
+//     registrations[0].showNotification("HELLO",{body:"Im sleepinh"});
+//     self.addEventListener('notificationonclick', function(event) {
+//         event.notification.close();
+//         clients.openWindow("https://youtu.be/PAvHeRGZ_lA");
+//     });
+//   });
+// Notification.requestPermission().then(function(permit){
+//     if(permit=='granted'){
+//         motification=new Notification("HELLO",{body:"Im dying"});
+//         motification.onclick = function(event) {
+//          event.preventDefault(); // prevent the browser from focusing the Notification's tab
+//          window.open('http://localhost/trial/notifiication/index.html?subject=DSGT&work=assignment&number=5&');
+//      }
+//     }
+// }
+// )
 var url=new URL(window.location.href);
 if(url.searchParams.get("subject")!=null && url.searchParams.get("work") && url.searchParams.get("number")){
     document.getElementById("subject").innerText=url.searchParams.get("subject");
     document.getElementById("work").innerText=url.searchParams.get("work")+" "+url.searchParams.get("number");
 }
+if ('showTrigger' in Notification.prototype) {
+    /* Notification Triggers supported */
+    console.log("supproted")
+  }else{
+      console.log("not supported")
+  }
